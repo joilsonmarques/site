ad940c767dd18789a4c6719caa7625c8bada8941
Joilson Marques <joilsonmarques@gmail.com>
2021-06-22 06:36:04 -0300

    Configurações básicas de theme para o styled-components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,27 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import { ThemeProvider } from 'styled-components'
 
 import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>A Nossa Casa de Podcasts</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="Somos um hub de ideias &amp; uma produtora de conteúdos em áudio."
         />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,54 @@
+export default {
+  grid: {
+    container: '130rem',
+    gutter: '3.2rem'
+  },
+  border: {
+    radius: '0rem'
+  },
+  font: {
+    family:
+      "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    normal: 400,
+    bold: 700,
+    sizes: {
+      xsmall: '1.5rem',
+      small: '1.8rem',
+      medium: '2.5rem',
+      large: '4.0rem',
+      xlarge: '5.0rem',
+      xxlarge: '6.0rem'
+    }
+  },
+  colors: {
+    white: '#FFFFFF',
+    black: '#000000',
+    lightGray: '#C4C4C4',
+    gray: '#8F8F8F',
+    darkGray: '#29292B',
+    blue: '#1C77C6',
+    lightBlue: '#5E7B93',
+    darkBlue: '#0E3C63',
+    orange: '#F95E3C',
+    red: '#C61C4A',
+    purple: '#451379',
+    lightPurple: '#9071AF',
+    darkPurple: '#3A0332'
+  },
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem'
+  },
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50
+  }
+}
