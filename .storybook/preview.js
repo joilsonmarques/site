18cb9740e51d30a475ed4352a9468b9ac8a6bffb
Joilson Marques <joilsonmarques@gmail.com>
2021-07-14 03:22:48 -0300

    Storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,22 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 
+export const parameters = {
+  backgrounds: {
+    default: 'ancp-light',
+    values: [
+      {
+        name: 'ancp-light',
+        value: theme.colors.white
+      },
+      {
+        name: 'ancp-dark',
+        value: theme.colors.darkGray
+      }
+    ]
+  }
+}
+
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
