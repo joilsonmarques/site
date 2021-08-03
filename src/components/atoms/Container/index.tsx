5f0d81011f255661c42c4d35433f99ed918e9404
Joilson Marques <joilsonmarques@gmail.com>
2021-08-02 23:17:58 -0300

    Fix to Container

--- src/components/atoms/Container/index.tsx
+++ src/components/atoms/Container/index.tsx
@@ -1,12 +1,8 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
 export const Container = styled.div`
-  ${({ theme }) => css`
-    width: 100%;
-    max-width: ${theme.grid.container};
-    margin-left: auto;
-    margin-right: auto;
-    padding-left: calc(${theme.grid.gutter} / 2);
-    padding-right: calc(${theme.grid.gutter} / 2);
-  `}
+  width: 100%;
+  overflow: hidden;
+  margin-left: auto;
+  margin-right: auto;
 `
