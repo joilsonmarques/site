362cad81c62a5a4c17cb03d5fdd459c1cd2ebad9
joilsonmarques <joilsonmarques@gmail.com>
2021-08-15 15:50:48 -0300

    Ajusts for Featured Podcast

--- src/components/FeaturedPodcast/mock.ts
+++ src/components/FeaturedPodcast/mock.ts
@@ -4,25 +4,20 @@ export default {
   description:
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
   podcast: {
-    cover: '/img/podcasts/calango-careta.jpeg',
     title: 'Calango Careta II',
     subtitle: 'Por MarcioRamos',
+    cover: '/img/podcasts/calango-careta.jpeg',
     description:
       'Sobre Brasília, música, cotidiano e demais atividades culturais aleatórias produzidas pelo Bloco do Calango Careta!',
     slug: 'calango-careta',
+    categories: [
+      { name: 'carnaval', slug: 'carnaval' },
+      { name: 'arte', slug: 'arte' },
+      { name: 'brasília', slug: 'brasilia' },
+      { name: 'música', slug: 'musica' }
+    ],
     lastEpisode: {
-      cover: '/img/podcasts/calango-careta.jpeg',
-      title: 'Introdução à astrologia',
-      slug: 'introducao-a-astrologia',
-      embed:
-        '<iframe src="https://open.spotify.com/embed/episode/7l6kyf9DCF9oTCAg3pzSnO" width="100%" height="152" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>',
-      embedUrl: 'https://open.spotify.com/embed/episode/7l6kyf9DCF9oTCAg3pzSnO',
-      categories: [
-        { name: 'carnaval', slug: 'carnaval' },
-        { name: 'arte', slug: 'arte' },
-        { name: 'brasília', slug: 'brasilia' },
-        { name: 'música', slug: 'musica' }
-      ]
+      embedUrl: 'https://open.spotify.com/embed/episode/7l6kyf9DCF9oTCAg3pzSnO'
     }
   }
 }
